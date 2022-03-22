fc271558f234b3f66c96067a6a7495344bafbce0
ww24 <ww24@users.noreply.github.com>
2022-03-23 00:28:29 +0900

    feat(terraform): add cloud scheduler job

---
+++ terraform/cloud_scheduler.tf
@@ -0,0 +1,28 @@
+resource "google_cloud_scheduler_job" "trigger" {
+  name             = "${var.name}-trigger"
+  description      = "${var.name} trigger"
+  schedule         = "0 * * * *"
+  time_zone        = "Asia/Tokyo"
+  attempt_deadline = "120s"
+
+  http_target {
+    http_method = "POST"
+    uri         = google_cloud_run_service.app.status[0].url
+    headers = {
+      content-type = "application/json"
+    }
+    body = base64encode(var.request_body)
+
+    oidc_token {
+      service_account_email = google_service_account.invoker.email
+      audience              = google_cloud_run_service.calendar_notifier.status[0].url
+    }
+  }
+
+  retry_config {
+    retry_count          = 5
+    min_backoff_duration = "1s"
+    max_backoff_duration = "10s"
+    max_doublings        = 2
+  }
+}
--- terraform/variables.tf
+++ terraform/variables.tf
@@ -36,3 +36,7 @@ variable "slack_token_secret" {
   type    = string
   default = "api-checker-slack-token"
 }
+
+variable "request_body" {
+  type = string
+}
